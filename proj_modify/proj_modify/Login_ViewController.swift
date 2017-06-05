e0fbb3b025e10f53c7a9baf9c808a7cfe5552c3c
sujin <sujin@sujinui-MacBook-Pro.local>
2017-06-06 02:40:50 +0900

    login with Firebase

---
+++ proj_modify/proj_modify/Login_ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  Login_ViewController.swift
+//  proj_modify
+//
+//  Created by sujin on 2017. 6. 5..
+//  Copyright © 2017년 sujin. All rights reserved.
+//
+
+import UIKit
+
+class Login_ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
