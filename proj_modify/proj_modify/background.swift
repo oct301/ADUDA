bf63a1ff7056a9040870460383c9c99ae3f9b21a
sujin <sujin@sujinui-MacBook-Pro.local>
2017-06-08 16:53:27 +0900

    new interface

---
+++ proj_modify/proj_modify/background.swift
@@ -0,0 +1,9 @@
+//
+//  background.swift
+//  proj_modify
+//
+//  Created by sujin on 2017. 6. 8..
+//  Copyright © 2017년 sujin. All rights reserved.
+//
+
+import Foundation
---
+++ proj_modify/proj_modify/new_loginViewController.swift
@@ -0,0 +1,35 @@
+//
+//  new_loginViewController.swift
+//  proj_modify
+//
+//  Created by sujin on 2017. 6. 7..
+//  Copyright © 2017년 sujin. All rights reserved.
+//
+
+import UIKit
+
+class new_loginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
